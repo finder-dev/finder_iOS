84e4b7e4c9daf6fe07a1bc249f4af38aa659fdda
seonyoung <4tjsdud2@naver.com>
2022-06-11 23:50:06 +0900

    F!nder 초기 세팅

---
+++ F!nder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F!nder
+//
+//  Created by 장선영 on 2022/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .orange
+    }
+    
+}
+
