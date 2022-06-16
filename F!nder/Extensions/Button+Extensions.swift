4c89a50e1a595e7eb7db2d308cf9d660031743a8
seonyoung <4tjsdud2@naver.com>
2022-06-16 22:17:27 +0900

    UI - 로그인

---
+++ F!nder/Extensions/Button+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Button+Extensions.swift
+//  F!nder
+//
+//  Created by 장선영 on 2022/06/16.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                        value: NSUnderlineStyle.single.rawValue,
+                                        range: NSRange(location: 0, length: title.count)
+            )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
--- F!nder/View/LoginViewController.swift
+++ F!nder/View/LoginViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 장선영 on 2022/06/16.
 //
 
+import Foundation
 import UIKit
 import SnapKit
 import Then
@@ -19,36 +20,77 @@ class LoginViewController: UIViewController {
         $0.image = UIImage(named: "main character")
     }
     
-    private lazy var kakaoLoginButton = UIButton(type: .custom).then {
-        $0.setTitle("카카오로 로그인", for: .normal)
-        $0.setImage(UIImage(named: "btn_login_kakao"), for: .normal)
+    private lazy var kakaoLoginView = UIView().then {
         $0.backgroundColor = UIColor(red:  254/255, green: 229/255, blue: 0/255, alpha: 1.0)
-        $0.setTitleColor(.black, for: .normal)
-        $0.titleLabel?.textAlignment = .center
-        $0.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        $0.addSubview(kakaoLoginLabel)
+        $0.addSubview(kakaoLoginImage)
+        $0.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapKakaoLogin))
+        $0.addGestureRecognizer(gesture)
     }
     
-    private lazy var appleLoginButton = UIButton(type: .custom).then {
-        $0.setTitle("애플로 로그인", for: .normal)
-        $0.setImage(UIImage(named: "btn_login_apple"), for: .normal)
+    private lazy var kakaoLoginImage = UIImageView().then {
+        $0.image = UIImage(named: "btn_login_kakao")
+    }
+    
+    private lazy var kakaoLoginLabel = UILabel().then {
+        $0.text = "카카오로 로그인"
+        $0.textAlignment = .center
+        $0.font = .systemFont(ofSize: 16.0, weight: .medium)
+        $0.textColor = .black
+    }
+    
+    private lazy var appleLoginView = UIView().then {
         $0.backgroundColor = .black
-        $0.setTitleColor(.white, for: .normal)
-        $0.titleLabel?.textAlignment = .center
-        $0.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        $0.addSubview(appleLoginLabel)
+        $0.addSubview(appleLoginImage)
+        $0.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapAppleLogin))
+        $0.addGestureRecognizer(gesture)
+    }
+
+    private lazy var appleLoginImage = UIImageView().then {
+        $0.image = UIImage(named: "btn_login_apple")
     }
     
-    private lazy var emailLoginButton = UIButton(type: .custom).then {
+    private lazy var appleLoginLabel = UILabel().then {
+        $0.text = "애플로 로그인"
+        $0.textAlignment = .center
+        $0.font = .systemFont(ofSize: 16.0, weight: .medium)
+        $0.textColor = .white
+    }
+    
+    private lazy var emailLoginButton = UIButton(type: .system).then {
         $0.setTitle("이메일로 로그인", for: .normal)
         $0.setTitleColor(.black, for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 14.0, weight: .medium)
+        $0.addTarget(self, action: #selector(didTapEmailLogin), for: .touchUpInside)
+        $0.setUnderline()
     }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         layout()
         attribute()
+        
+//        let tapApple = UITapGestureRecognizer(target: self, action: #selector(didTapAppleLogin))
+//        appleLoginView.addGestureRecognizer(tapApple)
 
+    }
+    
+    @objc func didTapKakaoLogin() {
+        print("didTapKakoLogin")
+    }
+    
+    @objc func didTapEmailLogin() {
+        print("didTapEmailLogin")
+    }
+    
+    @objc func didTapAppleLogin() {
+        print("didTapAppleLogin")
     }
 }
 
@@ -58,8 +100,8 @@ private extension LoginViewController {
         [
             logoImageView,
             characterImageView,
-            kakaoLoginButton,
-            appleLoginButton,
+            kakaoLoginView,
+            appleLoginView,
             emailLoginButton
         ].forEach {
             self.view.addSubview($0)
@@ -85,17 +127,44 @@ private extension LoginViewController {
             $0.width.equalTo(94.0)
         }
         
-        appleLoginButton.snp.makeConstraints {
+        appleLoginView.snp.makeConstraints {
             $0.bottom.equalTo(emailLoginButton.snp.top).inset(-4.0)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(327.0)
         }
         
-        kakaoLoginButton.snp.makeConstraints {
-            $0.bottom.equalTo(appleLoginButton.snp.top).inset(-12.0)
+        kakaoLoginView.snp.makeConstraints {
+            $0.bottom.equalTo(appleLoginView.snp.top).inset(-12.0)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(327.0)
         }
+        loginViewLayout()
+    }
+    
+    func loginViewLayout() {
+        
+        kakaoLoginLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(16.0)
+            $0.bottom.equalToSuperview().inset(16.0)
+        }
+        
+        kakaoLoginImage.snp.makeConstraints {
+            $0.centerY.equalTo(kakaoLoginView)
+            $0.leading.equalToSuperview().inset(16.0)
+        }
+        
+        appleLoginLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(16.0)
+            $0.bottom.equalToSuperview().inset(16.0)
+        }
+        
+        appleLoginImage.snp.makeConstraints {
+            $0.centerY.equalTo(appleLoginView)
+            $0.leading.equalToSuperview().inset(16.0)
+        }
+
     }
     
     func attribute() {
