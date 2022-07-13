9e93a5b8b9dd91d3c2a697e7d48c076f621839c8
seonyoung <4tjsdud2@naver.com>
2022-07-14 00:04:38 +0900

    UI - 토론 > 데이터 있는 경우

---
+++ F!nder/View/Main/Discuss/DiscussTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiscussTableViewCell.swift
+//  F!nder
+//
+//  Created by 장선영 on 2022/07/13.
+//
+
+import UIKit
+
+class DiscussTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
