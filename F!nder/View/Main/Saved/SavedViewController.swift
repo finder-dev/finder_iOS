5706ffa3d33a54f4a0451bbd12f09f2b391aa357
seonyoung <4tjsdud2@naver.com>
2022-07-16 17:59:30 +0900

    UI - 저장

--- F!nder/View/Main/Saved/SavedViewController.swift
+++ F!nder/View/Main/Saved/SavedViewController.swift
@@ -6,45 +6,100 @@
 //
 
 import UIKit
+enum dataStatus {
+    case noData
+    case yesData
+}
+class SavedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class SavedViewController: UIViewController {
+    let headerView = UIView()
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        setupHeaderView()
-        // Do any additional setup after loading the view.
+        layout()
+        attribute()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
+// tableview delegate, datasource
+extension SavedViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CommunityTableViewCell.identifier) as? CommunityTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.setupCellData()
+        return cell
+    }
+}
+
+private extension SavedViewController {
+    func layout() {
+        setupHeaderView()
+        yesDataView()
+//        noDataView()
+    }
+    
+    func attribute() {
+        
+    }
+}
 
 private extension SavedViewController {
     func setupHeaderView() {
-        let headerLabel = UILabel()
-        
-        self.view.addSubview(headerLabel)
+        self.view.addSubview(headerView)
         let safeArea = self.view.safeAreaLayoutGuide
-        headerLabel.snp.makeConstraints {
+        
+        headerView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(48.0)
             $0.top.equalTo(safeArea)
         }
+
+        let headerLabel = UILabel()
+        headerView.addSubview(headerLabel)
         
+        headerLabel.snp.makeConstraints {
+            $0.leading.trailing.top.bottom.equalToSuperview()
+        }
+
         headerLabel.text = "저장"
         headerLabel.font = .systemFont(ofSize: 16.0, weight: .bold)
         headerLabel.textColor = .blackTextColor
         headerLabel.textAlignment = .center
     }
+    
+    func noDataView() {
+        let noDataImageView = UIImageView()
+        noDataImageView.image = UIImage(named: "noSavedData")
+        
+        self.view.addSubview(noDataImageView)
+        
+        noDataImageView.snp.makeConstraints {
+            $0.width.equalTo(153.0)
+            $0.height.equalTo(133.0)
+            $0.top.equalTo(headerView.snp.bottom).offset(120.0)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    func yesDataView() {
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(headerView.snp.bottom).offset(16.0)
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CommunityTableViewCell.self, forCellReuseIdentifier: CommunityTableViewCell.identifier)
+    }
+    
 }
