feb1034baffb4b33d396d0d3bbfb885654ad368d
seonyoung <4tjsdud2@naver.com>
2023-03-12 01:07:43 +0900

    [#7] Refactor: FinderTextField 생성

--- F!nder/Presentation/Common/FinderTextField.swift
+++ F!nder/Presentation/Common/FinderTextField.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+final class FinderTextField: UITextField {
+    
+    public init(placeHolder: String) {
+        super.init(frame: .zero)
+        
+        self.placeholder = placeHolder
+        attribute()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func attribute() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.heightAnchor.constraint(equalToConstant: 54.0).isActive = true
+        self.layer.borderColor = UIColor.textFieldBorder.cgColor
+        self.layer.borderWidth = 1.0
+        self.addLeftPadding(padding: 20.0)
+    }
+}
