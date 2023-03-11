c93dbc448622ded59fd7f48046b19532e76c1127
seonyoung <4tjsdud2@naver.com>
2023-03-12 01:16:32 +0900

    [#7] Refactor: FinderButton 수정

--- F!nder/Presentation/Common/FinderButton.swift
+++ F!nder/Presentation/Common/FinderButton.swift
@@ -11,11 +11,12 @@ import UIKit
 final class FinderButton: UIButton {
     
     /// textColor = black, textAlignment = left로 default 설정
-    public init(buttonText: String, buttonHeight: CGFloat) {
+    public init(buttonText: String, buttonTitleFont: UIFont = .systemFont(ofSize: 16.0, weight: .medium), buttonHeight: CGFloat = 54.0) {
                     
         super.init(frame: .zero)
         
         self.setTitle(buttonText, for: .normal)
+        self.titleLabel?.font = buttonTitleFont
         self.heightAnchor.constraint(equalToConstant: buttonHeight).isActive = true
         
         attribute()
@@ -25,11 +26,22 @@ final class FinderButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override var isEnabled: Bool {
+        didSet {
+            if isEnabled {
+                self.backgroundColor = .mainTintColor
+                self.setTitleColor(.white, for: .normal)
+            } else {
+                self.backgroundColor = .grey7
+                self.setTitleColor(.grey8, for: .normal)
+            }
+        }
+    }
+    
     func attribute() {
         translatesAutoresizingMaskIntoConstraints = false
 
         self.backgroundColor = .mainTintColor
         self.setTitleColor(.white, for: .normal)
-        self.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
     }
 }
