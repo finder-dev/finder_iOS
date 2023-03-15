c4ef3a3f7f6e9f8941c35b61965746fff10424ba
seonyoung <4tjsdud2@naver.com>
2023-03-15 13:43:57 +0900

    [#2] Refactor: DebateModel 추가

---
+++ F!nder/Domain/Entities/DebateModel.swift
@@ -0,0 +1,14 @@
+//
+//  DebateModel.swift
+//  F!nder
+//
+//  Created by 장선영 on 2023/03/15.
+//
+
+import Foundation
+
+struct DebateModel {
+    var debateTitle: String = ""
+    var optionA: String = ""
+    var optionB: String = ""
+}
